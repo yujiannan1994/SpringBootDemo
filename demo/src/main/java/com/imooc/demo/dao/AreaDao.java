dbaf650157d109d44e4f1c7f3ec60c8ffbc45bc6
yujiannan1994 <yujiannan1994@gmail.com>
2018-03-29 20:29:26 +0800

    back-end supplement

---
+++ demo/src/main/java/com/imooc/demo/dao/AreaDao.java
@@ -0,0 +1,46 @@
+package com.imooc.demo.dao;
+
+import com.imooc.demo.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    /**
+     * 列出区域列表
+     *
+     * @return
+     */
+    List<Area> queryArea();
+
+    /**
+     * 根据Id列出具体区域
+     *
+     * @param areaId
+     * @return
+     */
+    Area queryAreaById(int areaId);
+
+    /**
+     * 插入区域信息
+     *
+     * @param area
+     * @return
+     */
+    int insertArea(Area area);
+
+    /**
+     * 更新区域信息
+     *
+     * @param area
+     * @return
+     */
+    int updateArea(Area area);
+
+    /**
+     * 删除区域信息
+     *
+     * @param areaId
+     * @return
+     */
+    int deleteArea(int areaId);
+}
